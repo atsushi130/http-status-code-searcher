31cda165e7fd8e64b823181488126e3e6367f0e3
atsushi130 <atsushi_main@outlook.jp>
2018-06-10 02:22:16 +0900

    remove: new method of HttpStatusCode

--- src/http_status_code_searcher/http_status_code.rs
+++ src/http_status_code_searcher/http_status_code.rs
@@ -1,16 +1,14 @@
 
+#[derive(RustcDecodable)]
+#[derive(Debug)]
+pub struct HttpStatusCodes {
+    pub values: Vec<HttpStatusCode>
+}
+
+#[derive(RustcDecodable)]
+#[derive(Debug)]
 pub struct HttpStatusCode {
     pub code: String,
     pub title: String,
     pub detail: String
-}
-
-impl HttpStatusCode {
-    pub fn new(code: &str, title: &str, detail: &str) -> HttpStatusCode {
-        HttpStatusCode {
-            code: code.to_string(),
-            title: title.to_string(),
-            detail: detail.to_string()
-        }
-    }
 }

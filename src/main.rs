bdb77649e2183877056a3c4c0f1b5672958f1ce2
atsushi130 <atsushi_main@outlook.jp>
2018-06-10 02:24:10 +0900

    implement: sample search code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 
+extern crate rustc_serialize;
+
 mod http_status_code_searcher;
 use http_status_code_searcher::HttpStatusCodeSearcher;
 
 fn main() {
-    let http_status_code = HttpStatusCodeSearcher.search_by("404");
-    println!("code: {}", http_status_code.code);
+    let http_status_code = HttpStatusCodeSearcher.search_by("200");
+    http_status_code.iter().for_each(|http_status_code| {
+        println!("{} {}", http_status_code.code, http_status_code.title);
+        println!("{}", http_status_code.detail);
+    });
 }

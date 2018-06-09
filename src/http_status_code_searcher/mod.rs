2ce081a66a2f9369a8aaf9d277bbe046fbd25f31
atsushi130 <atsushi_main@outlook.jp>
2018-06-10 02:23:26 +0900

    add: http status code repository to module

--- src/http_status_code_searcher/mod.rs
+++ src/http_status_code_searcher/mod.rs
@@ -1,6 +1,9 @@
 
 mod http_status_code;
-pub use self::http_status_code::HttpStatusCode;
+pub use self::http_status_code::{ HttpStatusCode, HttpStatusCodes };
 
 mod http_status_code_searcher;
 pub use self::http_status_code_searcher::HttpStatusCodeSearcher;
+
+mod http_status_code_repository;
+pub use self::http_status_code_repository::HttpStatusCodeRepository;

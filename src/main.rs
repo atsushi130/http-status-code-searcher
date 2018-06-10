38eb92c765e841c1c5cf0d70dec94fffe8937e72
atsushi130 <atsushi_main@outlook.jp>
2018-06-10 19:38:59 +0900

    implement: searchable http status code by command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 
 extern crate rustc_serialize;
 
+use std::env;
+
 mod http_status_code_searcher;
 use http_status_code_searcher::HttpStatusCodeSearcher;
 
 fn main() {
-    let http_status_code = HttpStatusCodeSearcher.search_by("200");
-    http_status_code.iter().for_each(|http_status_code| {
+
+    let args: Vec<String> = env::args().collect();
+
+    if args[1..].to_vec().is_empty() {
+        println!("not found arguments.");
+        return;
+    }
+
+    let http_status_code = &args[1];
+
+    let maybe_http_status_code = HttpStatusCodeSearcher.search_by(&http_status_code);
+    maybe_http_status_code.iter().for_each(|http_status_code| {
         println!("{} {}", http_status_code.code, http_status_code.title);
         println!("{}", http_status_code.detail);
     });
